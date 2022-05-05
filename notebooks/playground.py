e3dd0815983024a21576da7e29b3c0c5067da7a4
BarotsZimonczyk <bartosz.zimonczyk@gmail.com>
2022-05-06 00:36:36 +0200

    Playground update, it doesn't work

--- notebooks/playground.py
+++ notebooks/playground.py
@@ -16,12 +16,12 @@ model_2 = ErCaNet("Playground_2")
 model_2.load_state_dict(torch.load('models/CaptionEraseBZ-GPU-TheThird.pt'))
 model_2.eval()
 
-img_1 = ToTensor()(Image.open('notebooks/meme1.jpg'))
+img_1 = ToTensor()(Image.open('notebooks/orig_img_val_step_0.JPEG'))
 
-cleaned_img_1 = model_2.forward(img_1)
+cleaned_img_1 = model_2.forward(img_1[None, :])
 
 imshow(img_1.permute(1, 2, 0))
 show()
 
-imshow(cleaned_img_1.permute(1, 2, 0))
+imshow(cleaned_img_1.detach().numpy()[0].transpose(1, 2, 0))
 show()
